a7818a3d1244751371a13f97d9797be7e12768e6
Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
2022-08-25 16:07:24 +0600

    Release 22.8 (#44)
    * Release 22.8
    
    * Updated format jar for tests added
    
    * Fix security issue "`TrustManager` that accepts all certificates"
    
    Co-authored-by: Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
    
    Co-authored-by: ivankamkin <109517020+ivankamkin@users.noreply.github.com>

--- scripts/fix-region-point.bash
+++ scripts/fix-region-point.bash
@@ -5,4 +5,4 @@
 #
 set -euo pipefail
 
-find ./src -type f -iname 'RegionPoint.java' -exec sed -i '' -e 's_@SerializedName(value = "\(X\|Y\)"_@SerializedName(value = "\l\1"_g' '{}' \;
+find ./src -type f -iname 'RegionPoint.java' -exec sed -e 's_@SerializedName(value = "\(X\|Y\)"_@SerializedName(value = "\l\1"_g' -i '{}' \;
